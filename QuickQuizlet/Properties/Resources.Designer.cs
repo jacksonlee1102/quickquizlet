2c87f75b856bdaf6060691d0df8720ffbff5eeab
jacksonlee1102 <quyenka@gmail.com>
2017-03-24 11:45:36 +0900

    add runable file

--- QuickQuizlet/Properties/Resources.Designer.cs
+++ QuickQuizlet/Properties/Resources.Designer.cs
@@ -73,16 +73,6 @@ namespace QuickQuizlet.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap ico_setting {
-            get {
-                object obj = ResourceManager.GetObject("ico_setting", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap ico_setting_small {
             get {
                 object obj = ResourceManager.GetObject("ico_setting_small", resourceCulture);
